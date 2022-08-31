421c29b36f94240995ba01282696d21920958169
etienne-ly <111778487+etienne-ly@users.noreply.github.com>
2022-08-31 16:57:52 -0400

    updated

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ KEYWORDCOLOR = "#C678DD"
 STRINGCOLOR = "#98C379"
 INTCOLOR = "#E5C07B"
 FUNCCOLOR = "#61AFEF"
-FIRACODE = ("Fira Code", 12)
+FIRACODE = ("Consolas", 12)
 
 # initializing the tk window as root
 root = Tk()
@@ -17,9 +17,11 @@ root = Tk()
 # window configuration
 root.rowconfigure(0, weight=5)
 root.columnconfigure(0, weight=1)
+root.title("fast editor")
 
 # function to check syntax
 def check_syntax(*args):
+    get_cursor_position()
     lines = editor.get("1.0", END).split("\n")
     line = 0
     for code in lines:
@@ -48,6 +50,10 @@ def check_syntax(*args):
         matches = re.finditer(expression, code)
         for match in matches:
             editor.tag_add("func", f"{line}.{match.start()}", f"{line}.{match.end()}")
+            
+def get_cursor_position(*args):
+    curr_char = editor.index(INSERT).split(".")
+    char_lbl.config(text=f"line {curr_char[0]} : char {curr_char[1]}")
 
 # main frame containing the text editor
 main_frame = Frame(root, bg=BGCOLOR)
@@ -57,6 +63,32 @@ editor.tag_config("string", foreground=STRINGCOLOR)
 editor.tag_config("int", foreground=INTCOLOR)
 editor.tag_config("func", foreground=FUNCCOLOR)
 editor.pack(fill=BOTH, expand=True)
+
+# status bar containing current file and character
+status_frame = Frame(root, bg=STATUSCOLOR)
+file_lbl = Label(status_frame, text="no file loaded!", bg=STATUSCOLOR, fg=TEXTCOLOR, font=FIRACODE)
+file_lbl.pack(side=LEFT)
+char_lbl = Label(status_frame, text="line 1 : char 0", bg=STATUSCOLOR, fg=TEXTCOLOR, font=FIRACODE)
+char_lbl.pack(side=RIGHT)
+
+# command bar at the bottom
+command_frame = Frame(root, bg=BGCOLOR)
+command_lbl = Label(command_frame, text="command line : ", bg=BGCOLOR, fg=TEXTCOLOR, font=FIRACODE)
+command_lbl.pack(side=LEFT)
+command_txt = Entry(command_frame, bg=BGCOLOR, fg=TEXTCOLOR, font=FIRACODE, highlightthickness=0, relief=FLAT, insertbackground=TEXTCOLOR)
+command_txt.pack(side=RIGHT, fill=BOTH, expand=True)
+
+# placing the frames
+main_frame.grid(column=0, row=0, sticky="nsew")
+status_frame.grid(column=0, row=1, sticky="nsew")
+command_frame.grid(column=0, row=2, sticky="nsew")
+
+# checking the syntax written on each keypress
+root.bind("<KeyRelease>", check_syntax)
+root.bind("<Button-1>", get_cursor_position)
+
+# closing the main loop
+root.mainloop()
 
 # status bar containing current file and character
 status_frame = Frame(root, bg=STATUSCOLOR)
