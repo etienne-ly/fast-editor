d0936b7b8bd16f5492be8863b37fada445650fa0
etienne-ly <111778487+etienne-ly@users.noreply.github.com>
2022-09-06 15:06:46 -0400

    removed duplicates

--- main.py
+++ main.py
@@ -89,26 +89,3 @@ root.bind("<Button-1>", get_cursor_position)
 
 # closing the main loop
 root.mainloop()
-
-# status bar containing current file and character
-status_frame = Frame(root, bg=STATUSCOLOR)
-status_lbl = Label(status_frame, text="no file loaded!", bg=STATUSCOLOR, fg=TEXTCOLOR, font=FIRACODE)
-status_lbl.pack(side=LEFT)
-status_lbl = Label(status_frame, text="char 0 : 0", bg=STATUSCOLOR, fg=TEXTCOLOR, font=FIRACODE)
-status_lbl.pack(side=RIGHT)
-
-# command bar at the bottom
-command_frame = Frame(root, bg=BGCOLOR)
-command_lbl = Label(command_frame, text="command line :", bg=BGCOLOR, fg=TEXTCOLOR, font=FIRACODE)
-command_lbl.pack(side=LEFT)
-
-# placing the frames
-main_frame.grid(column=0, row=0, sticky="nsew")
-status_frame.grid(column=0, row=1, sticky="nsew")
-command_frame.grid(column=0, row=2, sticky="nsew")
-
-# checking the syntax written on each keypress
-root.bind("<KeyRelease>", check_syntax)
-
-# closing the main loop
-root.mainloop()
